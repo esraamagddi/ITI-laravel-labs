cc42dc97f08fa151d8b1bf9644d761b16b9900e6
esraamagddi <esraamagdy20133@gmail.com>
2024-04-30 10:33:47 +0300

    handle update error

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,7 +2,18 @@
 
 @section("content")
     <h1>update Post</h1>
+    @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     <form action="{{ route('post.update', $post['id']) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
         <label for="title">Title:</label><br>
         <input type="text" id="title" name="title" value='@php
             echo $post['title']
@@ -14,6 +25,13 @@
             echo $post['body']
         @endphp'
         ></input><br>
+        <label for="body">Description:</label><br>
+        <input type="text" id="body" name="description"
+        value='@php
+            echo $post['description']
+        @endphp'
+        ></input><br>
+
 
         <label for="image">Image:</label><br>
         <input type="file" id="image" name="image"><br>
